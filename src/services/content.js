fbc98256ec5bf4ffe3b7b515208d8c219a33b06f
Dylan Sharon <reachdylan@gmail.com>
2021-04-26 17:00:02 -0400

    define initial list of functions for basic CRUD and user auth functionality

---
+++ src/services/content.js
@@ -0,0 +1,51 @@
+//File containing and exporting functions regarding content and how it is displayed
+
+//function to get and store content locally
+function getContent() {
+    //get content
+    //store in array?
+    //return array
+}
+
+//function to add content
+function addContent() {
+    //show form for adding content
+    //get form content
+    //send form content to back end
+}
+
+//function to update content
+function editContent() {
+    //get target form content
+    //create form with content filled in
+    //setState to update content
+    //send updated content to back end
+}
+
+//function to delete content
+function deleteContent() {
+    //get target content
+    //send delete request to backend
+}
+
+//function to update state to only show content with certain tags
+function filterContent(/*tags*/) {
+    //take tags
+    //set state to only show content marked with tags
+}
+
+//function to favorite content
+function favContent() {
+    //get user
+    //get target content
+    //post target content to user favorites list
+    //update state to show content as favorite
+}
+
+//function to remove favorite content
+function removeFavContent() {
+    //get user
+    //get target
+    //post target content to remove from user favorites list
+    //update state to show content as not favorite
+}
---
+++ src/services/users.js
@@ -0,0 +1,28 @@
+//File containing and exporting functions regarding users and how it is displayed
+
+//function for creating a new user
+function createUser(/*parameters*/) {
+    //get user information
+    
+    //send user information to server
+
+}
+
+//function for deleting users
+function deleteUser() {
+    //get user information
+    
+    //send delete request to server
+}
+    
+//function to test if user is authorized
+function isAuth() {
+    //if user is authorized, return true
+    //else return false
+}
+
+//function to request authorization
+function requestAuth() {
+    //flag account to be marked as authorized to view restricted content or not
+    //
+}
