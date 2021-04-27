13f9440f33e1193ebacbd79fcd848bca7d21d3db
Kevin Meldau <me@kevinmeldau.com>
2021-04-27 12:49:15 -0400

    Correct merge conflict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from "react";
-import content from "./services/content";
-
-function App() {
-
-  useEffect(() => {
-    //call getContent
-    content.getContent();
-  }, [])
-
-=======
 // import { useState, useEffect } from "react";
 import Content from "./components/Content";
 import "./styles.css";
 
 export default function App() {
->>>>>>> km/styling
   return (
     <div className="container">
       <Content />
