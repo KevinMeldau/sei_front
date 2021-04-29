ec92e263ae219a0a7e25186bebbef1465dfb2619
Kevin Meldau <me@kevinmeldau.com>
2021-04-29 13:33:46 -0400

    Add form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import { useState, useEffect } from "react";
 import Public_facing from "./components/Public_facing";
+import Form from "./components/Form";
 import Member_content from "./components/Member_content";
 import Unit_one from "./components/Unit_one";
 import "./styles.css";
@@ -11,6 +12,7 @@ export default function App() {
       <div className="container">
         <Switch>
           <Route path="/member" component={Member_content} />
+          <Route path="/form" component={Form} />
           <Route path="/unit" component={Unit_one} />
           <Route path="/" component={Public_facing} />
         </Switch>
