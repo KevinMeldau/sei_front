8708162b3c0254b9a762ac62f695e477ed9347b8
Kevin Meldau <me@kevinmeldau.com>
2021-04-29 17:51:21 -0400

    fixed nav issue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../styles.css";
 import companyLogo from "../images/sei_logo.png";
 
 export default function Nav() {
   return (
-    <Router>
-      <div className="nav">
-        <div className="nav-items">
-          <img src={companyLogo} alt="Logo image" />
-          <Link to="/unit_one">Unit 1</Link>
-          <Link to="/unit_two">Unit 2</Link>
-          <Link to="/unit_three">Unit 3</Link>
-          <Link to="/unit_four">Unit 4</Link>
-        </div>
-        <div className="login-container">
-          <button>LOGIN</button>
-        </div>
+    <div className="nav">
+      <div className="nav-items">
+        <img src={companyLogo} alt="Logo image" />
+        <Link to="/unit_one">Unit 1</Link>
+        <Link to="/unit_two">Unit 2</Link>
+        <Link to="/unit_three">Unit 3</Link>
+        <Link to="/unit_four">Unit 4</Link>
       </div>
-    </Router>
+      <div className="login-container">
+        <button>LOGIN</button>
+      </div>
+    </div>
   );
 }
