9e62e3c10e9761af0d32aab2d08106939b524771
Kevin Meldau <me@kevinmeldau.com>
2021-04-28 21:07:19 -0400

    Fixed page routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Public_facing from "./components/Public_facing";
 import Member_content from "./components/Member_content";
 import Member_content_unit from "./components/Member_content_unit";
 import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="container">
-      <Public_facing />
-      <Member_content />
-      <Member_content_unit />
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path="/member" component={Member_content} />
+          <Route path="/unit" component={Member_content_unit} />
+          <Route path="/" component={Public_facing} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
