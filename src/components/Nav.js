68b7697e4b65405b235d54bca87b104b4d15c598
Kevin Meldau <me@kevinmeldau.com>
2021-04-30 12:26:03 -0400

    Update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,9 @@ export default function Nav() {
   return (
     <div className="nav">
       <div className="nav-items">
-        <img src={companyLogo} alt="Logo image" />
+        <Link to="/">
+          <img src={companyLogo} alt="Logo image" />
+        </Link>
         <Link to="/unit_one">Unit 1</Link>
         <Link to="/unit_two">Unit 2</Link>
         <Link to="/unit_three">Unit 3</Link>
