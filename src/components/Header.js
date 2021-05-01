235c75bf458c95b046ef2d6ce5ae403c3bc6d867
justinanmoreno <justinanmoreno@gmail.com>
2021-05-01 19:16:47 -0400

    firebase working...header needs work/refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles.css";
+import companyLogo from "../images/sei_logo.png";
+
 import { login, logout } from "../services/firebase";
 
-const Header = (props) =>(
-    <header>
-        <h1>SEI Resources</h1>
-        <ul>
-            <li>
-                Welcome,
-            </li>
-            <li>
-                Logout                
-            </li>
-            <li> onClick={login}Login</li>
-        </ul>
-    </header>
-);
-
-export default Header;
+export default function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav-items">
+        <Link to="/">
+          <img src={companyLogo} alt="Logo image" />
+        </Link>
+        <Link to="/unit_one">Unit 1</Link>
+        <Link to="/unit_two">Unit 2</Link>
+        <Link to="/unit_three">Unit 3</Link>
+        <Link to="/unit_four">Unit 4</Link>
+        <Link to="/external_Resources">External Resources</Link>
+      </div>
+      <div className="login-container">
+        <Link to="/form">
+          <button type="button">Upload</button>
+        </Link>
+          <ul>
+              
+              <li>Welcome,</li>
+              <li onClick={login}>Login</li>
+              <li onClick={logout}>Logout</li>
+          </ul>
+      </div>
+    </div>
+  );
+}
