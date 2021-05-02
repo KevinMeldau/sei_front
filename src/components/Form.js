5c39df2c6898f3fd662e044332647fdffe1e4783
Dylan Sharon <reachdylan@gmail.com>
2021-05-02 18:06:27 -0400

    changed placeholder to value for sake of stronger color

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,22 +11,22 @@ export default function Form(props) {
                 e.preventDefault();
                 contentServices.addContent();
             }}>
-        <input type="text" placeholder={props.location.state.content.title || "Title"} name="title" id="" />
-        <input type="text" placeholder={props.location.state.content.subject || "Subject"} name="subject" id="" />
-        <input type="text" placeholder={props.location.state.content.videoLink || "Video Link"} name="videoLink" id="" />
+        <input type="text" value={props.location.state.content.title || "Title"} name="title" id="" />
+        <input type="text" value={props.location.state.content.subject || "Subject"} name="subject" id="" />
+        <input type="text" value={props.location.state.content.videoLink || "Video Link"} name="videoLink" id="" />
         <input
           type="text"
-          placeholder={props.location.state.content.markdownLink || "Markdown Link"}
+          value={props.location.state.content.markdownLink || "Markdown Link"}
           name="markdownLink"
           id=""
         />
         <input
           type="text"
-          placeholder={props.location.state.content.resourceLinks[0] || "Resource Links"}
+          value={props.location.state.content.resourceLinks[0] || "Resource Links"}
           name="resourceLinks"
           id=""
         />
-        <input type="text" placeholder={props.location.state.content.tags[0] || "Tags"} name="tags" id="" />
+        <input type="text" value={props.location.state.content.tags[0] || "Tags"} name="tags" id="" />
         <button type="submit">Submit Data</button>
       </form>
     </div>
